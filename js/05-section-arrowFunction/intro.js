0876d59f6186b2e6a8cf1069a07fa392df568ebb
Jose Maria <chchema26@gmail.com>
2022-10-23 20:46:30 -0500

    Started section-05: Arrow functions

---
+++ js/05-section-arrowFunction/intro.js
@@ -0,0 +1,28 @@
+
+/**
+ * FUNCIONES DE FLECHA - ARROW FUNCTIONS
+ * Este tipo de funciones tienen un sintaxis variada
+ * que depende de lo que quieras realizar.
+ * 
+ * Normalmente tiene la misma estructura:
+ * 1. Función con argumentos.
+ * 2. Seguido de una flecha gorda.
+ * 3. Seguido del cuerpo de la función.
+ * 
+ * ¿PARA QUÉ SIRVEN?
+ * 1. Menos código que es más simple de interpretar.
+ * 2. No hay un nuevo "this" dentro de las funciones.
+ * 3. No hay constructores, no tiene new.
+ * 4. No puedes cambiar el valor del "this" aunque uses call(), apply() o bind().
+ * 5. No hay objeto "arguments"
+ * 6. Entre otras cosas.
+ * 
+ * SE COMPORTAN DE UNA FORMA MUY DIFERENTE A LAS FUNCIONES EN ECMASCRIPT 5.
+ * 
+ * 1. No hay creación de "this","super","arguments" y "new.target".
+ * 2. No puede ser llamado con new. 
+ * 3. No tienen prototipos (prototype).
+ * 4. No pueden cambiar "this".
+ * 5. No hay objecto "arguments".
+ * 6. No pueden tener nombre de parámetros duplicados. 
+ */
